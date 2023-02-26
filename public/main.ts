886a92e91275d288cee9fb1fc2e599d7f5f61caf
shynome <shynome@gmail.com>
2023-02-26 14:28:20 +0800

    direct import online js

--- public/main.ts
+++ public/main.ts
@@ -1,18 +1,11 @@
+import "https://go.dev/misc/wasm/wasm_exec.js";
+
 declare global {
   const Go: any;
   const GoFetch: typeof fetch;
 }
 
 Deno.test("go-fetch", async () => {
-  const p = Deno.run({
-    cmd: ["go", "env", "GOROOT"],
-    stdout: "piped",
-  });
-  let GoRoot = await p.output().then((buf) => new TextDecoder().decode(buf));
-  GoRoot = GoRoot.slice(0, -1 * "\n".length);
-
-  await import(`${GoRoot}/misc/wasm/wasm_exec.js`);
-
   const go = new Go();
 
   const wasmBuf = await Deno.readFile("./public/main.wasm");
@@ -28,7 +21,8 @@ Deno.test("go-fetch", async () => {
   const req = new Request("https://shyno.me");
   const r = await GoFetch(req);
 
-  await Promise.all([r.text(), p.close()]);
+  // clean
+  await Promise.all([r.text()]);
 
   if (r.status != 200) {
     throw new Error(`status is ${r.status}, expect 200`);
