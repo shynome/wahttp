58e8b75779792181611ab34767dfa4651efde9f2
shynome <shynome@gmail.com>
2023-02-28 01:19:53 +0800

    support js signal abort

--- request-in_js.go
+++ request-in_js.go
@@ -1,10 +1,14 @@
 package wahttp
 
 import (
+	"context"
+	"errors"
 	"io"
 	"net/http"
 	"syscall/js"
 )
+
+var ErrRequestAbortThroughJsSignal = errors.New("request abort through js signal")
 
 func JsRequest(r js.Value) (*http.Request, error) {
 
@@ -22,6 +26,14 @@ func JsRequest(r js.Value) (*http.Request, error) {
 	if err != nil {
 		return nil, err
 	}
+
+	ctx := req.Context()
+	ctx, cancel := context.WithCancelCause(ctx)
+	r.Get("signal").Set("onabort", js.FuncOf(func(this js.Value, args []js.Value) any {
+		cancel(ErrRequestAbortThroughJsSignal)
+		return nil
+	}))
+	req = req.WithContext(ctx)
 
 	headersIt := r.Get("headers").Call("entries")
 	for {
