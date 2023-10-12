39f924af51dbfb4e8f80e3de8beae121d48d675d
shynome <shynome@gmail.com>
2023-10-12 19:18:05 +0800

    replace lainio/err2 with shynome/err0

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/shynome/wahttp
 go 1.20
 
 require (
-	github.com/lainio/err2 v0.8.14
 	github.com/nlepage/go-js-promise v1.1.0
+	github.com/shynome/err0 v0.1.0
 )
--- public/main.go
+++ public/main.go
@@ -4,9 +4,9 @@ import (
 	"net/http"
 	"syscall/js"
 
-	"github.com/lainio/err2"
-	"github.com/lainio/err2/try"
 	promise "github.com/nlepage/go-js-promise"
+	"github.com/shynome/err0"
+	"github.com/shynome/err0/try"
 	"github.com/shynome/wahttp"
 )
 
@@ -20,13 +20,14 @@ func GoFetch() js.Func {
 	return js.FuncOf(func(this js.Value, args []js.Value) any {
 		p, resolve, reject := promise.New()
 
-		go func() {
-			defer err2.Catch(func(err error) {
+		go func() (err error) {
+			defer err0.Then(&err, nil, func() {
 				reject(err)
 			})
 			req := try.To1(wahttp.JsRequest(args[0]))
 			resp := try.To1(http.DefaultClient.Do(req))
 			resolve(wahttp.GoResponse(resp))
+			return
 		}()
 
 		return p
